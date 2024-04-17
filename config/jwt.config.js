544a4d46627a58338a4cb2a0964182f5f8177910
Motaz <mo3taz_hassis@yahoo.com>
2024-04-18 00:50:50 +0300

    Update jwt.config.js

--- config/jwt.config.js
+++ config/jwt.config.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken");
  
 module.exports.authenticate = (req, res, next) => {
+ console.log(req.cookies.usertoken)
+  console.log(process.env.SECRET_KEY)
   jwt.verify(req.cookies.usertoken, process.env.SECRET_KEY, (err, payload) => {
     if (err) { 
       console.log('no token')
